fa672f6323b47e2fafece20e4a04409ec070e12e
Robin Anil <r@emorahealth.com>
2025-02-23 08:05:02 -0500

    Add posthog to the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_PsGQf2yEDp6dUbqGcLbCMiBWpLfY2JAtlTCsLWGXxkK',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
+
 createRoot(document.getElementById("root")!).render(<App />);
